c070305bfceac4ce8104477846d2ab005d94d0c1
Jan <itbeheer@itkiss.nl>
2021-12-04 10:30:57 +0100

    No beautiful code, but it is working :)

---
+++ src/test/nl/krebos/aoc2021/puzzle04/Puzzle04Test.java
@@ -0,0 +1,41 @@
+package nl.krebos.aoc2021.puzzle04;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Puzzle04Test {
+  
+  @Test
+  void test1ref() {
+    Puzzle04 pzzl = new Puzzle04("Giant Squid", 4, "1ref");
+    pzzl.setInputFile("data/puzzle04a.txt");
+    long result = pzzl.part1();
+    assertEquals(4512,result);
+  }
+  
+  @Test
+  void test1() {
+    Puzzle04 pzzl = new Puzzle04("Giant Squid", 4, "1");
+    pzzl.setInputFile("data/puzzle04b.txt");
+    long result = pzzl.part1();
+    assertEquals(54275,result);
+  }  
+  
+  @Test
+  void test2ref() {
+    Puzzle04 pzzl = new Puzzle04("Giant Squid", 4, "2ref");
+    pzzl.setInputFile("data/puzzle04a.txt");
+    long result = pzzl.part2();
+    assertEquals(1924,result);
+  }  
+  
+  @Test
+  void test2() {
+    Puzzle04 pzzl = new Puzzle04("Giant Squid", 4, "2");
+    pzzl.setInputFile("data/puzzle04b.txt");
+    long result = pzzl.part2();
+    assertEquals(13158,result);
+  }  
+}
