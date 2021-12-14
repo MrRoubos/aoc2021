cdd6b7def3f6f553d5465dab60e5b39b2bd94d11
Jan <itbeheer@itkiss.nl>
2021-12-14 16:25:47 +0100

    WIP: puzzle14, part2

--- src/main/nl/krebos/aoc2021/Puzzle14.java
+++ src/main/nl/krebos/aoc2021/Puzzle14.java
@@ -12,6 +12,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
 
 public class Puzzle14 extends Puzzle {
   int steps;
@@ -19,6 +20,16 @@ public class Puzzle14 extends Puzzle {
   Map<String, String> piRules = new HashMap<>();
   Set<String> quantity = new HashSet<>();
   Map<String, Integer> totals = new HashMap<>();
+  Stack<String> list = new Stack<>();
+  Stack<String> list2 = new Stack<>();
+  String[] list3=null;
+  
+  public static void main (String [] args) {
+    Puzzle14 pzl = new Puzzle14("bla", 14, 2, "data/puzzle14a2.txt");
+    pzl.steps=20;
+    pzl.execute();
+    
+  }
   
   public Puzzle14(String name, int day, int part, String file) {
     super(name, day, part, file);    
@@ -48,7 +59,7 @@ public class Puzzle14 extends Puzzle {
       String stepResult = processStep();
       poltempl = stepResult;
       // prl("After step : " + (i + 1) + ": " +  stepResult);
-      countUniques();
+      
       List<Integer> totalList = new ArrayList<>();
       for (String key : totals.keySet()) {
         prl (key + " = " + totals.get(key));
@@ -102,7 +113,6 @@ public class Puzzle14 extends Puzzle {
         sb.append(element);
         quantity.add(poltempl.substring(i,i+1));
         quantity.add(element);
-        //sb.append(poltempl.substring(i+1,i+2));
       }
     }    
     sb.append(poltempl.substring(poltempl.length()-1, poltempl.length()));
@@ -113,9 +123,71 @@ public class Puzzle14 extends Puzzle {
     long result = 0L;   
     readFile();
     
+    // perhaps via ArrayList or Stack ? instead of a String?
+    for(int i=0; i<poltempl.length()-1;i++) {
+      list.add(poltempl.substring(i,i+2));
+    }
+    //list2 = new Stack<>();
+    list3 = list.toArray(new String[0]);
+    prl("list3: " + list3.length);
+    for (int i=0; i<steps; i++) {
+      processStep2();
+//      pr(list2.get(0).substring(0,1));
+//      for (String key : list2) {
+//        pr(key.substring(1,2));
+//      }
+//      prl("");
+      list3 = list2.toArray(new String[0]);
+      
+      // prl("After step : " + (i + 1) + ": " +  stepResult);
+      //countUniques();
+    }          
+    prl("unique values: " + quantity.size());
+    countUniques2();
+    List<Integer> totalList = new ArrayList<>();
+    for (String key : totals.keySet()) {
+      prl (key + " = " + totals.get(key));
+      totalList.add(totals.get(key));
+    }
+    Collections.sort(totalList);
+    result = totalList.get(totalList.size()-1) - totalList.get(0);
     return result;
   }
 
+  private void processStep2() {
+    String element=null;            
+    for(String check : list3) {
+      //prl("Check: " +check);
+      element = piRules.get(check);
+      if (element != null) {
+        list2.push(check.substring(0,1) + element);
+        list2.push(element + check.substring(1,2));
+        
+        quantity.add(check.substring(0,1));
+        quantity.add(element);        
+      }
+    }
+  }
+
+  private void countUniques2() {
+    Iterator<String> it = quantity.iterator();
+    String check=null;
+    int count=0;
+        
+    while (it.hasNext()) {
+      count=0;
+      check = it.next();
+      for (String key : list3) {
+        if (key.substring(1,2).equals(check)) {
+          count++;          
+        }
+      }
+      totals.put(check, count);
+    }
+    check = null;
+  }
+
+  
   private void readFile() {
     String input = "";
         
--- src/test/nl/krebos/aoc2021/Puzzle14Test.java
+++ src/test/nl/krebos/aoc2021/Puzzle14Test.java
@@ -23,12 +23,27 @@ public class Puzzle14Test {
   
   
   @Test
-  void test2() {
-    Puzzle14 pzzl = new Puzzle14("Extended Polymerization", 14, 1, "data/puzzle14a1.txt");
-    //pzzl.steps = 40;
+  void test2a() {
+    Puzzle14 pzzl = new Puzzle14("Extended Polymerization", 14, 2, "data/puzzle14a1.txt");
     pzzl.steps = 10;
     long result = pzzl.execute();
-    //assertEquals(814,result);
+    assertEquals(1588,result);
   }  
+  @Test
+  void test2b() {
+    Puzzle14 pzzl = new Puzzle14("Extended Polymerization", 14, 2, "data/puzzle14a2.txt");
+    pzzl.steps = 10;
+    long result = pzzl.execute();
+    assertEquals(3306,result);
+  }  
+  
+  @Test
+  void test2c() {
+    Puzzle14 pzzl = new Puzzle14("Extended Polymerization", 14, 2, "data/puzzle14a2.txt");
+    pzzl.steps = 20;
+    long result = pzzl.execute();
+    //assertEquals(3306,result);
+  }  
+  
 }
 
